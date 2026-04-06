513e83e176d347979cc1a8bed0bdbdf1ddc89006
Matheus Rodrigues <mathteusrrt1@gmail.com>
2026-04-06 16:28:29 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/matheus/usuario/business/UsuarioService.java
+++ src/main/java/com/matheus/usuario/business/UsuarioService.java
@@ -3,8 +3,12 @@ package com.matheus.usuario.business;
 import com.matheus.usuario.business.converter.UsuarioConverter;
 import com.matheus.usuario.business.dto.UsuarioDTO;
 import com.matheus.usuario.infrastructure.entity.UsuarioEntity;
+import com.matheus.usuario.infrastructure.exceptions.ConflictException;
+import com.matheus.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.matheus.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +17,38 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter converter;
+    private final PasswordEncoder passwordEncoder;
 
 
-    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         UsuarioEntity usuario = converter.paraUsuario(usuarioDTO);
         return converter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
+
+    public void emailExiste(String email) {
+
+        boolean existe = verificaEmailExistente(email);
+        if (existe) {
+            throw new ConflictException("Email já cadastrado: " + email);
+        }
+
+    }
+
+    public boolean verificaEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    public UsuarioEntity buscaUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado " + email)
+        );
+    }
+
+    public void deletaUsuarioPorEmail(String email) {
+        usuarioRepository.deleteByEmail(email);
+    }
+
 
 }
--- src/main/java/com/matheus/usuario/controller/UsuarioController.java
+++ src/main/java/com/matheus/usuario/controller/UsuarioController.java
@@ -2,23 +2,52 @@ package com.matheus.usuario.controller;
 
 import com.matheus.usuario.business.UsuarioService;
 import com.matheus.usuario.business.dto.UsuarioDTO;
+import com.matheus.usuario.infrastructure.entity.UsuarioEntity;
+import com.matheus.usuario.infrastructure.repository.UsuarioRepository;
+import com.matheus.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(name = "/usuario")
+@RequestMapping("/usuario")
 @RequiredArgsConstructor
 public class UsuarioController {
 
     private final UsuarioService service;
+    public final AuthenticationManager authenticationManager;
+    public final JwtUtil jwtUtil;
+    private final UsuarioRepository usuarioRepository;
+
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity.ok(service.salvaUsuario(usuarioDTO));
     }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(), usuarioDTO.getSenha())
+        );
+        return "Bearer " + jwtUtil.generateToken(usuarioDTO.getEmail());
+    }
+
+    @GetMapping
+    public ResponseEntity<UsuarioEntity> buscaUsuarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(service.buscaUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable("email") String email) {
+        service.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
